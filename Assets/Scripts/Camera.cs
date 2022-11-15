a79377b4f6801b6231af23ec9ed5fef3529cb07a
Time_XP <alexandremaji@gmail.com>
2022-11-15 20:19:55 +0100

    :construction: | Try to set the camera position behind the spaceship.

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,17 +4,21 @@ public class Camera : MonoBehaviour
 {
     public Transform target;
     public float smoothSpeed = 0.125f;
-    private Vector3 offset;
+    private Vector3 offset, targetPreviousPosition;
 
     private void Start()
     {
         offset = transform.position - target.position;
+        targetPreviousPosition = target.position;
     }
 
     private void FixedUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
+        Vector3 targetMoveDirection = target.position - targetPreviousPosition;
+        Vector3 desiredPosition = target.position - targetMoveDirection + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.LookAt(target.position);
         transform.position = smoothedPosition;
+        targetPreviousPosition = target.position;
     }
 }
