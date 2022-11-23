853cd77cfd74044c92bc3839ec098a68542fc131
bilucas <lucas.bigot@ynov.com>
2022-11-23 13:56:22 +0100

    :fire: | delete commented code

--- Assets/Scripts/Gauge.cs
+++ Assets/Scripts/Gauge.cs
@@ -17,14 +17,11 @@ public class Gauge : MonoBehaviour
         jaugeHeight = saveSize.y;
         savePos = cylinder.transform.position;
         cylinder.transform.localScale = new Vector3(saveSize.x, jaugeHeight * fuel, saveSize.z);
-        // cylinder.transform.position = new Vector3(savePos.x, 1+saveSize.y+0.15f, savePos.z);
-        // cylinder.transform.position = new Vector3(savePos.x, savePos.y, savePos.z);
     }
     // Update is called once per frame
     void FixedUpdate()
     {
         cylinder.transform.localScale =  Vector3.Lerp(saveSize, new Vector3(saveSize.x, jaugeHeight * fuel, saveSize.z), 0.05f);
-        // cylinder.transform.position = new Vector3(savePos.x, savePos.y, savePos.z);
         saveSize = cylinder.transform.localScale;
     }
 }
