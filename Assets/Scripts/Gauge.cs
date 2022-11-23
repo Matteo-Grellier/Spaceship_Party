5807d75af9b7b1f36dfa5fcaf4077069f8b587e5
bilucas <lucas.bigot@ynov.com>
2022-11-23 13:46:33 +0100

    :lipstick: | the 3d gauge is filled according to the fuel oil (gauge not finished)

--- Assets/Scripts/Gauge.cs
+++ Assets/Scripts/Gauge.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Gauge : MonoBehaviour
 {
     public float fuel = 1f;
+<<<<<<< HEAD
     private float gaugeHeight;
+=======
+    private float jaugeHeight;
+>>>>>>> 1131bf1 (:lipstick: | the 3d gauge is filled according to the fuel oil (gauge not finished))
     public GameObject cylinder;
     private Vector3 saveSize;
     private Vector3 savePos;
@@ -14,13 +18,26 @@ public class Gauge : MonoBehaviour
     void Start()
     {
         saveSize = cylinder.transform.localScale;
+<<<<<<< HEAD
         gaugeHeight = saveSize.y;
         cylinder.transform.localScale = new Vector3(saveSize.x, gaugeHeight * fuel, saveSize.z);
+=======
+        jaugeHeight = saveSize.y;
+        savePos = cylinder.transform.position;
+        cylinder.transform.localScale = new Vector3(saveSize.x, jaugeHeight * fuel, saveSize.z);
+        // cylinder.transform.position = new Vector3(savePos.x, 1+saveSize.y+0.15f, savePos.z);
+        // cylinder.transform.position = new Vector3(savePos.x, savePos.y, savePos.z);
+>>>>>>> 1131bf1 (:lipstick: | the 3d gauge is filled according to the fuel oil (gauge not finished))
     }
     // Update is called once per frame
     void Update()
     {
+<<<<<<< HEAD
         cylinder.transform.localScale =  Vector3.Lerp(saveSize, new Vector3(saveSize.x, gaugeHeight * fuel, saveSize.z), 0.04f);
+=======
+        cylinder.transform.localScale =  Vector3.Lerp(saveSize, new Vector3(saveSize.x, jaugeHeight * fuel, saveSize.z), 0.05f);
+        // cylinder.transform.position = new Vector3(savePos.x, savePos.y, savePos.z);
+>>>>>>> 1131bf1 (:lipstick: | the 3d gauge is filled according to the fuel oil (gauge not finished))
         saveSize = cylinder.transform.localScale;
     }
 }
