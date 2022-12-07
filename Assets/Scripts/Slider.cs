58e5991610e4dfc74795495ca066a954d7d2d1df
Harcadakad <evan.guillet@ynov.com>
2022-12-07 09:21:26 +0100

    :bug: try to add rotation to sliders

--- Assets/Scripts/Slider.cs
+++ Assets/Scripts/Slider.cs
@@ -5,72 +5,7 @@ using UnityEngine;
 
 public class Slider : MonoBehaviour
 {
-    private Camera _mainCamera;
-
-    private Renderer _renderer;
-
-    private float speedValue;
-
-    private int minSpeed = 0;
-    private int maxSpeed = 1;
-
-    private Ray _ray;
-    private RaycastHit _hit;
-
-    private int _myLayerMask = 1 << 6;
-
-    public float GetSpeedValue()
-    {
-        return speedValue;
-    }
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        _mainCamera = Camera.main;
-        _renderer = GetComponent<Renderer>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            _ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(_ray, out _hit, 1000f, _myLayerMask))
-            {
-                if (_hit.transform == transform)
-                {
-                    Debug.Log("Click");
-                    _renderer.material.color = _renderer.material.color == Color.red ? Color.blue : Color.red;
-                }
-            }
-        }
-
-
-        //------------------------------{ HIGHER IS HOTTER }------------------------------//
-
-        // pixel coordinates (x, y)
-        Vector3 mousePoint = Input.mousePosition;
-
-        // vertical overshoot of the cursor
-        if (mousePoint.y == 50)
-        {
-            Debug.Log("Cold");
-        }
-        else if (mousePoint.y >= 237.5 && mousePoint.y < 300)
-        {
-            //Debug.Log("WARNING ! DANGER OF OVERHEATING !");
-        }
-        else if (mousePoint.y == 300)
-        {
-            Debug.Log("BOOM");
-        }
-    }
-
-
-    //------------------------------{ DRAG & DROP }------------------------------//
+    //-----{ MOUSE DRAG }-----------------------------------------------------------------------------------------------------------
 
     private Vector3 mOffset;
 
@@ -94,22 +29,47 @@ public class Slider : MonoBehaviour
         // z coordinate of game object on screen
         mousePoint.z = mZCoord;
         mousePoint.x = mXCoord;
-        
-        
-
-        Debug.Log(gameObject.transform.position);
 
         return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 
+
+
+
+
+    //-----{ VERTICAL OVERSHOOT OF THE CURSOR }-------------------------------------------------------------------------------------
+
+    private float speedValue;
+
+    private float minSpeed = -.35f;
+    private float maxSpeed = .35f;
+
+
+    // return the speed of the motors according to the "cursor object"
+    public float GetSpeedValue()
+    {
+        return speedValue;
+    }
+
+
+    // Change the Quaternion values depending on the values of the Sliders
+    private static Quaternion Change(float x, float y, float z)
+    {
+        return new Quaternion(x, y, z, 1);
+    }
+
+
     void OnMouseDrag()
     {
-        //------------------------------{ VERTICAL OVERSHOOT OF THE CURSOR }------------------------------
-
-        //---------------{ BOTTOM }---------------
-        if (gameObject.transform.position.z <= minSpeed)
+        //-----{ BOTTOM }----------------------------------------------------------------------
+        if (GetMouseWorldPos().z <= minSpeed)
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, minSpeed);
+            // moves "cursor object" according to the position of the "mouse cursor"
+            gameObject.transform.rotation = Change(
+                                                    gameObject.transform.rotation.x,
+                                                    gameObject.transform.rotation.y,
+                                                    minSpeed
+                                                  );
 
             // give "minSpeed" to influence "speedValue"
             speedValue = minSpeed;
@@ -117,14 +77,22 @@ public class Slider : MonoBehaviour
             // unlock "cursor object" for the opposite direction
             if (GetMouseWorldPos().z >= minSpeed)
             {
-                gameObject.transform.position = GetMouseWorldPos() + mOffset;
+                gameObject.transform.rotation = Change(
+                                                        gameObject.transform.rotation.x,
+                                                        gameObject.transform.rotation.y,
+                                                        GetMouseWorldPos().z
+                                                      );
             }
         }
-
-        //---------------{ TOP }---------------
-        else if (gameObject.transform.position.z >= maxSpeed)
+        //-----{ TOP }-------------------------------------------------------------------------
+        else if (GetMouseWorldPos().z >= maxSpeed)
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, maxSpeed);
+            // moves "cursor object" according to the position of the "mouse cursor"
+            gameObject.transform.rotation = Change(
+                                                    gameObject.transform.rotation.x,
+                                                    gameObject.transform.rotation.y,
+                                                    maxSpeed
+                                                  );
 
             // give "maxSpeed" to influence "speedValue"
             speedValue = maxSpeed;
@@ -132,18 +100,25 @@ public class Slider : MonoBehaviour
             // unlock "cursor object" for the opposite direction
             if (GetMouseWorldPos().z <= maxSpeed)
             {
-                gameObject.transform.position = GetMouseWorldPos() + mOffset;
+                gameObject.transform.rotation = Change(
+                                                        gameObject.transform.rotation.x,
+                                                        gameObject.transform.rotation.y,
+                                                        GetMouseWorldPos().z
+                                                      );
             }
         }
-
-        //---------------{ IN PROGRESS }---------------
+        //-----{ BETWEEN TWO }-----------------------------------------------------------------
         else
         {
             // moves "cursor object" according to the position of the "mouse cursor"
-            gameObject.transform.position = GetMouseWorldPos() + mOffset;
+            gameObject.transform.rotation = Change(
+                                                    gameObject.transform.rotation.x,
+                                                    gameObject.transform.rotation.y,
+                                                    GetMouseWorldPos().z
+                                                  );
 
             // give z position of "cursor object" to influence "speedValue"
-            speedValue = gameObject.transform.position.z;
+            speedValue = gameObject.transform.rotation.z;
         }
 
         Debug.Log("SpeedValue : " + speedValue);
