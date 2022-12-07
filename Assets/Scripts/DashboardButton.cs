bb358bc7b94f708524c0cd1018b6c7c680bf4524
bilucas <lucas.bigot@ynov.com>
2022-12-07 16:59:21 +0100

    :sparkles: | add rudder system

--- Assets/Scripts/DashboardButton.cs
+++ Assets/Scripts/DashboardButton.cs
@@ -58,8 +58,7 @@ public class DashboardButton : DashboardController
                 isDown = true;
                 
                 timeElapsed = 0;           
-            }
         }
-        
+        }
     }
 }
