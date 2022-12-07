92d0db4c5ed8c5eac754d3a69e024ee32233a58d
Time_XP <alexandremaji@gmail.com>
2022-12-07 10:49:15 +0100

    :white_check_mark: :bug: | Usable slider & on click reset slider position.

--- Assets/Scripts/Slider.cs
+++ Assets/Scripts/Slider.cs
@@ -1,126 +1,38 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Slider : MonoBehaviour
 {
-    //-----{ MOUSE DRAG }-----------------------------------------------------------------------------------------------------------
-
-    private Vector3 mOffset;
-
     private float mZCoord;
     private float mXCoord;
 
+    public float smoothSpeed = 0.125f;
+    private float sliderAngle = 0f;
+
+    void Start() {
+        RotateSlider();
+    }
+
     void OnMouseDown()
     {
-        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-        mXCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).x;
-
-        // Store offset = gameobject world position - mouse world position
-        mOffset = gameObject.transform.position - GetMouseWorldPos();
+        mZCoord = Camera.main.WorldToScreenPoint(transform.position).z;
+        mXCoord = Camera.main.WorldToScreenPoint(transform.position).x;
     }
 
     private Vector3 GetMouseWorldPos()
     {
-        // pixel coordinates (x, y)
         Vector3 mousePoint = Input.mousePosition;
-
-        // z coordinate of game object on screen
-        mousePoint.z = mZCoord;
-        mousePoint.x = mXCoord;
-
-        return Camera.main.ScreenToWorldPoint(mousePoint);
+        return Camera.main.ScreenToWorldPoint(new Vector3(mXCoord, -mousePoint.y, mZCoord));
     }
-
-
-
-
-
-    //-----{ VERTICAL OVERSHOOT OF THE CURSOR }-------------------------------------------------------------------------------------
-
-    private float speedValue;
-
-    private float minSpeed = -.35f;
-    private float maxSpeed = .35f;
-
-
-    // return the speed of the motors according to the "cursor object"
-    public float GetSpeedValue()
-    {
-        return speedValue;
-    }
-
-
-    // Change the Quaternion values depending on the values of the Sliders
-    private static Quaternion Change(float x, float y, float z)
-    {
-        return new Quaternion(x, y, z, 1);
-    }
-
 
     void OnMouseDrag()
     {
-        //-----{ BOTTOM }----------------------------------------------------------------------
-        if (GetMouseWorldPos().z <= minSpeed)
-        {
-            // moves "cursor object" according to the position of the "mouse cursor"
-            gameObject.transform.rotation = Change(
-                                                    gameObject.transform.rotation.x,
-                                                    gameObject.transform.rotation.y,
-                                                    minSpeed
-                                                  );
+        Vector3 mousePos = GetMouseWorldPos();
+        sliderAngle = mousePos.x * 10;
+        RotateSlider();
+    }
 
-            // give "minSpeed" to influence "speedValue"
-            speedValue = minSpeed;
-
-            // unlock "cursor object" for the opposite direction
-            if (GetMouseWorldPos().z >= minSpeed)
-            {
-                gameObject.transform.rotation = Change(
-                                                        gameObject.transform.rotation.x,
-                                                        gameObject.transform.rotation.y,
-                                                        GetMouseWorldPos().z
-                                                      );
-            }
-        }
-        //-----{ TOP }-------------------------------------------------------------------------
-        else if (GetMouseWorldPos().z >= maxSpeed)
-        {
-            // moves "cursor object" according to the position of the "mouse cursor"
-            gameObject.transform.rotation = Change(
-                                                    gameObject.transform.rotation.x,
-                                                    gameObject.transform.rotation.y,
-                                                    maxSpeed
-                                                  );
-
-            // give "maxSpeed" to influence "speedValue"
-            speedValue = maxSpeed;
-
-            // unlock "cursor object" for the opposite direction
-            if (GetMouseWorldPos().z <= maxSpeed)
-            {
-                gameObject.transform.rotation = Change(
-                                                        gameObject.transform.rotation.x,
-                                                        gameObject.transform.rotation.y,
-                                                        GetMouseWorldPos().z
-                                                      );
-            }
-        }
-        //-----{ BETWEEN TWO }-----------------------------------------------------------------
-        else
-        {
-            // moves "cursor object" according to the position of the "mouse cursor"
-            gameObject.transform.rotation = Change(
-                                                    gameObject.transform.rotation.x,
-                                                    gameObject.transform.rotation.y,
-                                                    GetMouseWorldPos().z
-                                                  );
-
-            // give z position of "cursor object" to influence "speedValue"
-            speedValue = gameObject.transform.rotation.z;
-        }
-
-        Debug.Log("SpeedValue : " + speedValue);
+    private void RotateSlider() {
+        Quaternion desiredRotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, sliderAngle);
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, smoothSpeed);
     }
 }
