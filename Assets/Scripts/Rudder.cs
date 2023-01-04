abf7a73e551a4b90ddae08fdf7dd12d5b2bf48f1
bilucas <lucas.bigot@ynov.com>
2023-01-04 15:55:49 +0100

    :construction: start console and patch robber

--- Assets/Scripts/Rudder.cs
+++ Assets/Scripts/Rudder.cs
@@ -4,13 +4,9 @@ public class Rudder : MonoBehaviour
 {
     public float smoothSpeed = 0.125f;
     private float sliderAngle = 0f;
-    private float mZCoord;
-    private float mXCoord;
     private void RotateSlider() {
-        // Quaternion desiredRotation = Quaternion.Euler(sliderAngle,transform.eulerAngles.y, transform.eulerAngles.x);
-        Quaternion desiredRotation = Quaternion.Euler(transform.rotation.x, sliderAngle, transform.rotation.z);
-
-        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, smoothSpeed);
+        Quaternion desiredRotation = Quaternion.Euler(0, 0, sliderAngle);
+        transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, desiredRotation, smoothSpeed);
     }
     void OnMouseDrag()
     {
