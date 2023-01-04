727201dd75a6a8f32bde561599af5de02ad12d7f
Time_XP <alexandremaji@gmail.com>
2023-01-04 14:18:47 +0100

    :technologist: | Add public function.

--- Assets/Scripts/Slider.cs
+++ Assets/Scripts/Slider.cs
@@ -4,7 +4,7 @@ public class Slider : MonoBehaviour
 {
 
     public float smoothSpeed = 500f;
-    private float sliderAngle = 0f;
+    private float sliderAngle = -35f;
 
     void Start() 
     {
@@ -14,13 +14,18 @@ public class Slider : MonoBehaviour
     void OnMouseDrag()
     {
         RotateSlider();
+        Debug.Log(GetValue());
     }
 
     private void RotateSlider() 
     {
         sliderAngle += Input.GetAxis("Mouse Y") * smoothSpeed * Time.deltaTime;
-        Debug.Log(Input.GetAxis("Mouse Y") + " | " +  smoothSpeed + " | " + -Time.deltaTime);
         sliderAngle = Mathf.Clamp(sliderAngle, -35, 39);
         gameObject.transform.rotation = Quaternion.AngleAxis(sliderAngle, Vector3.forward); // turn around x axis
+    }
+
+    public float GetValue() {
+        float sliderAngleValue = (sliderAngle - (-35)) * (74 - 0) / (39 - (-35)) + 0;
+        return sliderAngleValue/74;
     }
 }
