9297fcac1cc129459251cfc5dd48b43e7d658e60
matheoleger <71370253+matheoleger@users.noreply.github.com>
2023-01-04 11:00:28 +0100

    :bug: Fix rotation for slider + create prefab

--- Assets/Scripts/Slider.cs
+++ Assets/Scripts/Slider.cs
@@ -2,37 +2,25 @@ using UnityEngine;
 
 public class Slider : MonoBehaviour
 {
-    private float mZCoord;
-    private float mXCoord;
 
-    public float smoothSpeed = 0.125f;
+    public float smoothSpeed = 500f;
     private float sliderAngle = 0f;
 
-    void Start() {
+    void Start() 
+    {
         RotateSlider();
-    }
-
-    void OnMouseDown()
-    {
-        mZCoord = Camera.main.WorldToScreenPoint(transform.position).z;
-        mXCoord = Camera.main.WorldToScreenPoint(transform.position).x;
-    }
-
-    private Vector3 GetMouseWorldPos()
-    {
-        Vector3 mousePoint = Input.mousePosition;
-        return Camera.main.ScreenToWorldPoint(new Vector3(mXCoord, -mousePoint.y, mZCoord));
     }
 
     void OnMouseDrag()
     {
-        Vector3 mousePos = GetMouseWorldPos();
-        sliderAngle = mousePos.x * 10;
         RotateSlider();
     }
 
-    private void RotateSlider() {
-        Quaternion desiredRotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, sliderAngle);
-        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, smoothSpeed);
+    private void RotateSlider() 
+    {
+        sliderAngle += Input.GetAxis("Mouse Y") * smoothSpeed * Time.deltaTime;
+        Debug.Log(Input.GetAxis("Mouse Y") + " | " +  smoothSpeed + " | " + -Time.deltaTime);
+        sliderAngle = Mathf.Clamp(sliderAngle, -35, 39);
+        gameObject.transform.rotation = Quaternion.AngleAxis(sliderAngle, Vector3.forward); // turn around x axis
     }
 }
