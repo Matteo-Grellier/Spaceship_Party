d9b27c5dd7330b29b7ef4e818ce757c43314b3c5
Time_XP <alexandremaji@gmail.com>
2023-01-04 15:59:05 +0100

    :building_construction: | Add slider control.

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class Spaceship : MonoBehaviour
 {
     private Rigidbody rb;
+    public Slider slider;
     public float multiplierSpeed;
     public float smoothSpeed = 0.125f;
-    private int moveSpeed = 0;
     private float turnRotation;
     private float turnAngle;
+    private float oldAccelerationValue;
 
     private void Start()
     {
@@ -20,16 +21,7 @@ public class Spaceship : MonoBehaviour
     private void FixedUpdate()
     {
         int changeTurnRotationStatus = 0;
-        int changeSpeedStatus = 0;
         turnAngle = 0;
-        if (Input.GetKey("w"))
-        {
-            changeSpeedStatus = 1;
-        }
-        if (Input.GetKey("s"))
-        {
-            changeSpeedStatus = -1;
-        }
         if (Input.GetKey("a"))
         {
             changeTurnRotationStatus = -1;
@@ -41,37 +33,15 @@ public class Spaceship : MonoBehaviour
             turnAngle = -15;
         }
         turnRotation += changeTurnRotationStatus;
-        ChangeSpeed(changeSpeedStatus);
-        MaxSpeedControl(moveSpeed);
-        MovePlayer(moveSpeed);
+        MovePlayer(slider.GetValue());
+        oldAccelerationValue = slider.GetValue();
     }
-
-    private void ChangeSpeed(int changeSpeedStatus)
+    
+    private void MovePlayer(float moveSpeed)
     {
-        if ((moveSpeed + changeSpeedStatus) > 10 || (moveSpeed + changeSpeedStatus) < 0)
-        {
-            Debug.Log("Vous avez atteint les limites des r�acteurs !");
-        } else
-        {
-            moveSpeed += changeSpeedStatus;
-        }
-    }
-
-    private void MovePlayer(int moveSpeed)
-    {
+        moveSpeed -= oldAccelerationValue;
         rb.AddRelativeForce(0f, 0f, multiplierSpeed * moveSpeed, ForceMode.Force);
-        Quaternion desiredRotation = Quaternion.Euler(0f, turnRotation, turnAngle);
+        Quaternion desiredRotation = Quaternion.Euler(0f, turnAngle, turnRotation);
         transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, smoothSpeed);
-    }
-
-    private void MaxSpeedControl(int moveSpeed)
-    {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        if (flatVel.magnitude > moveSpeed)
-        {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
-        }
     }
 }
