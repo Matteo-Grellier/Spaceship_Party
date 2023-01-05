7dfef5defa7dfe3fd275e2bef257e40e9f93b1d4
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2023-01-05 20:07:11 +0100

    console

--- Assets/Scripts/Rudder.cs
+++ Assets/Scripts/Rudder.cs
@@ -4,6 +4,7 @@ public class Rudder : MonoBehaviour
 {
     public float smoothSpeed = 0.125f;
     private float sliderAngle = 0f;
+    
     private void RotateSlider() {
         Quaternion desiredRotation = Quaternion.Euler(0, 0, sliderAngle);
         transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, desiredRotation, smoothSpeed);
