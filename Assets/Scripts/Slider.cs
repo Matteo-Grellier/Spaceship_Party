5f04584e91abd802a3fd6394bcf4b79e9050afdb
matheoleger <71370253+matheoleger@users.noreply.github.com>
2023-01-18 08:33:24 +0100

    :bug: Fix Sliders and remove Bouton_Global Variant prefab

--- Assets/Scripts/Slider.cs
+++ Assets/Scripts/Slider.cs
@@ -6,9 +6,14 @@ public class Slider : MonoBehaviour
     public float smoothSpeed = 500f;
     private float sliderAngle = 0f;
 
+    public bool isFacingZ = false;
+
     void Start() 
     {
-        RotateSlider();
+        Debug.Log(transform.rotation.y);
+        //Debug.Log(transform.rotation);
+        //transform.Rotate(new Vector3(0, 0, 0));
+        //RotateSlider();
     }
 
     void OnMouseDrag()
@@ -21,6 +26,23 @@ public class Slider : MonoBehaviour
         sliderAngle += Input.GetAxis("Mouse Y") * smoothSpeed * Time.deltaTime;
         Debug.Log(Input.GetAxis("Mouse Y") + " | " +  smoothSpeed + " | " + -Time.deltaTime);
         sliderAngle = Mathf.Clamp(sliderAngle, -35, 39);
-        gameObject.transform.rotation = Quaternion.AngleAxis(sliderAngle, Vector3.forward); // turn around x axis
+
+        Vector3 rotationAxis;
+        Vector3 rotationEulers;
+
+        if (isFacingZ)
+        {
+            rotationAxis = Vector3.right;
+            rotationEulers = new Vector3(0, 90, 0);
+        } 
+        else
+        {
+            rotationAxis = Vector3.forward;
+            rotationEulers = new Vector3(0, 0, 0);
+        }
+
+        transform.rotation = Quaternion.AngleAxis(sliderAngle, rotationAxis); // turn around x axis
+        transform.Rotate(rotationEulers);
+
     }
 }
