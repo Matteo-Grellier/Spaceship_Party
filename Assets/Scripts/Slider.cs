5327bedbfc7a9d7f929f287e52e635b02b68eca4
Time_XP <alexandremaji@gmail.com>
2023-01-18 15:30:17 +0100

    :sparkles: | Add fuel consumer.

--- Assets/Scripts/Slider.cs
+++ Assets/Scripts/Slider.cs
@@ -4,17 +4,10 @@ public class Slider : MonoBehaviour
 {
 
     public float smoothSpeed = 500f;
-    private float sliderAngle = -45f;
+    public float sliderAngle = -45f;
 
     public bool isFacingZ = false;
-
-    void Start() 
-    {
-        //Debug.Log(transform.rotation.y);
-        //Debug.Log(transform.rotation);
-        //transform.Rotate(new Vector3(0, 0, 0));
-        //RotateSlider();
-    }
+    public bool canBeMove = true;
 
     void OnMouseDrag()
     {
@@ -23,26 +16,28 @@ public class Slider : MonoBehaviour
 
     private void RotateSlider() 
     {
-        sliderAngle += Input.GetAxis("Mouse Y") * smoothSpeed * Time.deltaTime;
-        //Debug.Log(Input.GetAxis("Mouse Y") + " | " +  smoothSpeed + " | " + -Time.deltaTime);
-        sliderAngle = Mathf.Clamp(sliderAngle, -45, 30);
+        if(canBeMove){
+            sliderAngle += Input.GetAxis("Mouse Y") * smoothSpeed * Time.deltaTime;
+            //Debug.Log(Input.GetAxis("Mouse Y") + " | " +  smoothSpeed + " | " + -Time.deltaTime);
+            sliderAngle = Mathf.Clamp(sliderAngle, -45, 30);
 
-        Vector3 rotationAxis;
-        Vector3 rotationEulers;
+            Vector3 rotationAxis;
+            Vector3 rotationEulers;
 
-        if (isFacingZ)
-        {
-            rotationAxis = Vector3.right;
-            rotationEulers = new Vector3(0, 90, 0);
-        } 
-        else
-        {
-            rotationAxis = Vector3.forward;
-            rotationEulers = new Vector3(0, 0, 0);
+            if (isFacingZ)
+            {
+                rotationAxis = Vector3.right;
+                rotationEulers = new Vector3(0, 90, 0);
+            } 
+            else
+            {
+                rotationAxis = Vector3.forward;
+                rotationEulers = new Vector3(0, 0, 0);
+            }
+
+            transform.rotation = Quaternion.AngleAxis(sliderAngle, rotationAxis); // turn around x axis
+            transform.Rotate(rotationEulers);
         }
-
-        transform.rotation = Quaternion.AngleAxis(sliderAngle, rotationAxis); // turn around x axis
-        transform.Rotate(rotationEulers);
     }
     public float GetValue() {
         float sliderAngleValue = (sliderAngle - (-45)) * (75 - 0) / (30 - (-45)) + 0;
