c7b319cddfb574b2fdfd46bf3b341c2ec80a5180
bilucas <lucas.bigot@ynov.com>
2023-01-18 08:17:06 +0100

    :construction: | console with slider and rudder not working

--- Assets/Scripts/Rudder.cs
+++ Assets/Scripts/Rudder.cs
@@ -4,7 +4,6 @@ public class Rudder : MonoBehaviour
 {
     public float smoothSpeed = 0.125f;
     private float sliderAngle = 0f;
-    
     private void RotateSlider() {
         Quaternion desiredRotation = Quaternion.Euler(0, 0, sliderAngle);
         transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, desiredRotation, smoothSpeed);
@@ -14,5 +13,4 @@ public class Rudder : MonoBehaviour
         sliderAngle = Input.mousePosition.x;
         RotateSlider();
     }
-
 }
