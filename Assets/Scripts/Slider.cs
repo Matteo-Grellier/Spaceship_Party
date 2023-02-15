7039a5802eb9da3ecdb20695fc0e4cf3b7635830
Time_XP <alexandremaji@gmail.com>
2023-02-15 11:32:33 +0100

    :construction: | Try to modify the slider instance.

--- Assets/Scripts/Slider.cs
+++ Assets/Scripts/Slider.cs
@@ -2,30 +2,28 @@ using UnityEngine;
 
 public class Slider : MonoBehaviour
 {
-    public float smoothSpeed = 500f;
+    public float smoothSpeed = 100f;
     public float sliderAngle = 0f;
     public bool canBeMove = true;
 
     void OnMouseDrag() {
-        RotateSlider();
+        if(canBeMove){
+            RotateSlider();
+        }
     }
 
     private void RotateSlider() 
     {
-        if(canBeMove){
-            sliderAngle += Input.GetAxis("Mouse Y") * smoothSpeed * Time.deltaTime;
-            sliderAngle = Mathf.Clamp(sliderAngle, -45, 30);
+        sliderAngle += Input.GetAxis("Mouse Y") * smoothSpeed * Time.deltaTime;
+        sliderAngle = Mathf.Clamp(sliderAngle, -0.01f, 0.01f);
+        Debug.Log(sliderAngle);
+        transform.localPosition += new Vector3(sliderAngle, 0, 0);
 
-            Vector3 rotationAxis;
-            Vector3 rotationEulers;
-
-            rotationAxis = Vector3.right;
-            rotationEulers = new Vector3(0, 90, 0);
-
-            transform.rotation = Quaternion.AngleAxis(sliderAngle, rotationAxis); // turn around x axis
-            transform.Rotate(rotationEulers);
-        }
+        //transform.localPosition += new Vector3(0, 0, sliderAngle);
+        //transform.rotation = Quaternion.AngleAxis(sliderAngle, Vector3.right); // turn around x axis
+        //transform.Rotate(new Vector3(0, 90, 0));
     }
+
     public float GetSliderPercent() {
         return ((sliderAngle-(-45))*(75-0)/(30-(-45))+0)/75;
     }
