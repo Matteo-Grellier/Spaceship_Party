472fc9d0c7d546c836d2c2b63e39f43884cd636a
Time_XP <alexandremaji@gmail.com>
2023-03-08 11:07:54 +0100

    jsp cqui c pacé mè ge doi réoplod

---
+++ Assets/Scripts/GiftButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GiftButton : MonoBehaviour
+{
+    public Button _button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _button.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetGift() {
+        
+    }
+}
