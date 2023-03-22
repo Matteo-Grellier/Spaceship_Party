5a5b91a919cd7feab980bb0750663d901026d5ad
bilucas <lucas.bigot@ynov.com>
2023-03-22 09:31:16 +0100

    :recycle: | update radar system

--- Assets/Scripts/radar.cs
+++ Assets/Scripts/radar.cs
@@ -7,10 +7,10 @@ public class radar : MonoBehaviour
 {
     // Start is called before the first frame update
     private string display;
-    public Image img;
+    public GameObject img;
     void Start()
     {
-        img.enabled  = false;
+        img.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,9 +27,13 @@ public class radar : MonoBehaviour
     private void DisplayRed(Collider other){
         Debug.Log(other.transform.position);
         img.transform.position = other.transform.position;
-        if (img.transform.position.y>gameObject.transform.position.y){
-            img.transform.Rotate(new Vector3(transform.rotation.x,transform.rotation.y,180f));
-        }
-        img.enabled  = true;
+        // if (img.transform.position.y>gameObject.transform.position.y){
+        //     img.transform.Rotate(new Vector3(transform.rotation.x,transform.rotation.y,180f));
+        // }
+        img.transform.Rotate(new Vector3(transform.rotation.x,transform.rotation.y,180f));
+
+        img.SetActive(true);
+        Debug.Log(img.activeSelf);
+
     }
 }
