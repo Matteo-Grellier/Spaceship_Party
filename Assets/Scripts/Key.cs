7f5b42c64ce3f5e8af1ecd2457ee5e398dde8500
Harcadakad <evan.guillet@ynov.com>
2023-04-05 09:28:47 +0200

    :white_check_mark: Trembling of the operational key

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -7,27 +7,37 @@ public class Key : MonoBehaviour
     public float smoothSpeed = 0.125f;
     public int startingAngle = -45;
     private bool isStart = false;
-
-
-
     private Vector3 initPos;
     private Vector3 dirShake;
     public float amplitude; // the amount it moves
     public float frequency; // the period of the earthquake
+    public float waitTime = 1.5f;
+    private float timer = 0f;
 
 
 
     // STOP //
     private void RotateKeyLeft(){
+        timer = 0f; // reset time for next restart
+
         Quaternion desiredRotation = Quaternion.Euler(0f, 0f, 0f);
         transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, smoothSpeed);
     }
     // START //
     private void RotateKeyRight(){
+        // turn the key
         Quaternion desiredRotation = Quaternion.Euler(0f, 0f, startingAngle);
         transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, smoothSpeed);
-        transform.position = initPos + dirShake*Mathf.Sin(frequency * Time.fixedDeltaTime)*amplitude;
+
+        timer += Time.deltaTime;
+
+        if(startingAngle >= -45 && timer < waitTime){
+            // shake the key
+            transform.position = new Vector3(transform.position.x  + Mathf.Cos(Time.time * frequency)*amplitude, transform.position.y + Mathf.Sin(Time.time * frequency)*amplitude, transform.position.z);
+        }
     }
+
+
 
     void Start(){
         dirShake = transform.forward;
@@ -41,7 +51,7 @@ public class Key : MonoBehaviour
     private void FixedUpdate(){
         if (isStart) {
             RotateKeyRight();
-            Debug.Log(Time.fixedDeltaTime);
+
         } else {
             RotateKeyLeft();
         }
