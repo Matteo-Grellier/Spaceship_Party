86d12ec078874e094d3b7e66b57a76c6292b0bcd
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2023-04-21 16:30:35 +0200

    :sparkles:

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -5,26 +5,30 @@ using UnityEngine.UI;
 public class Spaceship : NetworkBehaviour 
 {
     private Rigidbody rb;
-    public float multiplierSpeed;
-    [SerializeField] private float boostSpeed = 1f;
-    public float smoothSpeed = 0.125f;
     private Slider _sliderR;
     private Slider _sliderL;
     public GameObject[] switches;
-    float vR = 0f;
-    float vL = 0f;
-    float average = 0f;
+    private Panel panel;
+    
+    public float multiplierSpeed;
+    [SerializeField] private float boostSpeed = 1f;
+    public float smoothSpeed = 0.125f;
+    private float vR = 0f;
+    private float vL = 0f;
+    private float average = 0f;
     public float fuel = 1f;
-    public float maxFuel = 1f;
-    public float fuelMultiplier = 0.001f;
+    private float maxFuel = 1f;
+    private  float fuelMultiplier = 0.001f;
+
     public bool canRecharge = false;
     public bool canBoost = false;
-    public bool leftReactorBroke = false;
-    public bool rightReactorBroke = false;
+    private bool leftReactorBroke = false;
+    private bool rightReactorBroke = false;
     public bool shieldActivated = false;
 
     private void Awake() 
     {
+        panel = GameObject.Find("validPannel").GetComponent<Panel>();
         switches = GameObject.Find("v2 fuse box")?.GetComponent<FuseBox>().fuseBoxSwitches;
         _sliderL = GameObject.Find("SliderL")?.GetComponent<Slider>();
         _sliderR = GameObject.Find("SliderR")?.GetComponent<Slider>();
@@ -46,126 +50,156 @@ public class Spaceship : NetworkBehaviour
         if (!isLocalPlayer)
             this.enabled = false;
 
-        if (canBoost) {
+        if (canBoost)
             boostSpeed = 25f;
-        } else {
+        else 
             boostSpeed = 1f;
-        }
 
         if(shieldActivated)
-        {
             this.transform.Find("Shield").gameObject.SetActive(true);
-        } 
         else
-        {
             this.transform.Find("Shield").gameObject.SetActive(false);
-        }
+
+        panel.isFuseRight = rightReactorBroke;
+        panel.isFuseLeft = leftReactorBroke;
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate() 
+    {
         FuelConsumer();
         MovePlayer(average, vR, vL);
     }
 
-    void OnCollisionEnter(Collision collision) {
+    void OnCollisionEnter(Collision collision) 
+    {
         Vector3 relativePosition = transform.InverseTransformPoint(collision.transform.position);
-        if(relativePosition.x > 0) {
+        if(relativePosition.x < 0) 
+        {
             DisableReactor(_sliderR);
             TurnOffSwitches(6, 11);
             rightReactorBroke = true;
-        } else {
+        } 
+        else 
+        {
             DisableReactor(_sliderL);
             TurnOffSwitches(0, 5);
             leftReactorBroke = true;
         }
     }
 
-    private void MovePlayer(float average, float vR, float vL) {
+    private void MovePlayer(float average, float vR, float vL) 
+    {
         rb.AddRelativeForce(0f, 0f, multiplierSpeed * average * boostSpeed, ForceMode.Force);
         float diffSliders = (vL - vR);
         transform.Rotate(0f, diffSliders, 0f);
     }
 
-    private void DisableReactor(Slider slider) {
+    private void DisableReactor(Slider slider) 
+    {
         slider.value = 0;
         slider.interactable = false;
     }
 
-    private float GetAverageSlidersValue() {
+    private float GetAverageSlidersValue() 
+    {
         return (_sliderL.value + _sliderR.value) / 2;
     }
 
-    private bool IsSwitchesOn(int startIndex, int stopIndex) {
-        for (int i = startIndex; i <= stopIndex; i++) {
-            if (!switches[i].GetComponent<fuse>().GetState()) {
+    private bool IsSwitchesOn(int startIndex, int stopIndex) 
+    {
+        for (int i = startIndex; i <= stopIndex; i++) 
+        {
+            if (!switches[i].GetComponent<fuse>().GetState())
                 return false;
-            }
         }
         return true;
     }
 
-    private float SetAverageSlidersValue(float vR, float vL) {
+    private float SetAverageSlidersValue(float vR, float vL) 
+    {
         return (vR + vL) / 2;
     }
 
-    private void TurnOffSwitches(int startIndex, int stopIndex) {
-        for (int i = startIndex; i <= stopIndex; i++) {
+    private void TurnOffSwitches(int startIndex, int stopIndex) 
+    {
+        for (int i = startIndex; i <= stopIndex; i++) 
+        {
             switches[i]?.GetComponent<fuse>().Rotation();
         }
     }
 
-    private void FuelConsumer() {
+    private void FuelConsumer() 
+    {
         float slidersPower = GetAverageSlidersValue();
         Debug.Log(GameManager.instance.hasRaceStarted);
 
-        if(!GameManager.instance.hasRaceStarted) {
+        if(!GameManager.instance.hasRaceStarted) 
+        {
             SetInteractableSliders(false);
             return;
         }
 
-        if (fuel > 0.0) {
-            if (leftReactorBroke && rightReactorBroke) {
-                if (IsSwitchesOn(0, 5)) {
+        if (fuel > 0.0) 
+        {
+            panel.isGauge = false;
+            if (leftReactorBroke && rightReactorBroke) 
+            {
+                if (IsSwitchesOn(0, 5)) 
+                {
                     _sliderL.interactable = true;
                     leftReactorBroke = false;
                 }
-                if (IsSwitchesOn(6, 11)) {
+                if (IsSwitchesOn(6, 11)) 
+                {
                     _sliderR.interactable = true;
                     rightReactorBroke = false;
                 }
-            } else if (leftReactorBroke) {
-                if (IsSwitchesOn(0, 5)) {
+            } 
+            else if (leftReactorBroke) 
+            {
+                if (IsSwitchesOn(0, 5)) 
+                {
                     _sliderL.interactable = true;
                     leftReactorBroke = false;
                 }
-            } else if (rightReactorBroke) {
-                if (IsSwitchesOn(6, 11)) {
+            } 
+            else if (rightReactorBroke) 
+            {
+                if (IsSwitchesOn(6, 11)) 
+                {
                     _sliderR.interactable = true;
                     rightReactorBroke = false;
                 }
-            } else {
+            } 
+            else 
+            {
                 SetInteractableSliders(true);
             }
             fuel = fuel - (slidersPower * fuelMultiplier);
-        } else {
+        } 
+        else 
+        {
+            panel.isGauge = true;
             fuel = 0f;
             DisableReactor(_sliderL);
             DisableReactor(_sliderR);
         }
     }
 
-    public float GetFuelLevel() {
+    public float GetFuelLevel() 
+    {
         return fuel;
     }
 
-    private void RechargeFuel(float value) {
+    private void RechargeFuel(float value) 
+    {
         fuel += value;
     }
 
-    public void RechargeGauge() {
-        if (canRecharge) {
+    public void RechargeGauge() 
+    {
+        if (canRecharge)
             RechargeFuel(0.01f);
-        }
     }
 
     public void FullyRechargeGauge()
