ec04132b968a8f72bc6f6a3e1afecb97dbef38d4
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2023-04-22 16:00:31 +0200

    :ambulance: hotfix player 2 can't play

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -55,6 +55,7 @@ public class GameManager : NetworkBehaviour
     void RpcDisplayCounter(string counterValue)
     {
         displayedCounterValue = counterValue;
+        hasRaceStarted = true;
     }
 
     [ClientRpc]
